7327f28de831ff2186075f5b85856be86aa69ef5
himanshu131098 <himanshuvairagade1310@gmail.com>
2020-07-24 17:37:13 +0530

    Updates

---
+++ precache-manifest.5b48fbff2a8e6a76b3899242d290ce85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61acb67489115a76f2fd33c1d3e257d",
+    "url": "/trial/index.html"
+  },
+  {
+    "revision": "54f4d6a6da82a2992965",
+    "url": "/trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f46cb924f69c5af6976a",
+    "url": "/trial/static/js/2.7110949a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trial/static/js/2.7110949a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f4d6a6da82a2992965",
+    "url": "/trial/static/js/main.8c68964c.chunk.js"
+  },
+  {
+    "revision": "b88a2bf5dbee4ad325d4",
+    "url": "/trial/static/js/runtime-main.64ac26fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
