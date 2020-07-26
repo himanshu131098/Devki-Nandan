535e958c05c52f361a6ae1e35d9ec816ccd1bb9a
himanshu131098 <himanshuvairagade1310@gmail.com>
2020-07-26 16:09:22 +0530

    NavBar closing on click

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-
-
-
-
-
-
 
 const navbarStyle={
     navDiv:{
@@ -27,16 +21,21 @@ const navbarStyle={
 }
 
 function NavigationBar(){
+
+    const [expanded,setExpanded] = useState(false);
+
+
+
     return(
         
-        <Navbar  style={navbarStyle.navDiv} sticky="top" bg="dark" expand="lg">
+        <Navbar expanded={expanded} style={navbarStyle.navDiv} sticky="top" bg="dark" expand="lg">
         <Navbar.Brand  style={navbarStyle.brand}>Devki Nandan</Navbar.Brand>
-        <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end navbar-collapse">
-        <Nav.Link style={navbarStyle.link} href="#home">Home</Nav.Link>
-        <Nav.Link style={navbarStyle.link} href="#layout">Layout</Nav.Link>
-        <Nav.Link style={navbarStyle.link} href="#location">Location</Nav.Link>
-        <Nav.Link style={navbarStyle.link} href="#contact">Contact-Us</Nav.Link>
+        <Nav.Link onClick={() => setExpanded(false)} style={navbarStyle.link} href="#home">Home</Nav.Link>
+        <Nav.Link onClick={() => setExpanded(false)} style={navbarStyle.link} href="#layout">Layout</Nav.Link>
+        <Nav.Link onClick={() => setExpanded(false)} style={navbarStyle.link} href="#location">Location</Nav.Link>
+        <Nav.Link onClick={() => setExpanded(false)} style={navbarStyle.link} href="#contact">Contact-Us</Nav.Link>
         </Navbar.Collapse>
         </Navbar>
         
